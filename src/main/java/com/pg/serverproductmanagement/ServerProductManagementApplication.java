5aad5a642d14b9b2b5e2018d6f815770bbaca260
Paulo Gomes <gomes7.paulo@gmail.com>
2022-06-14 22:46:27 -0300

    Initial commit

---
+++ src/main/java/com/pg/serverproductmanagement/ServerProductManagementApplication.java
@@ -0,0 +1,13 @@
+package com.pg.serverproductmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerProductManagementApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pg/serverproductmanagement/model/Role.java
@@ -0,0 +1,5 @@
+package com.pg.serverproductmanagement.model;
+
+public enum Role {
+    USER, ADMIN
+}
